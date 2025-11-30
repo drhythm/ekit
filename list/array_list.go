8464cb87748f130bcb5763a426655974349eed3b
drhythm <drcsscs@gmail.com>
2025-11-30 13:17:01 +0800

    feat(list): implement Insert and Range metheds, refactor error handling.
    This commit adds new functionality to ArrayList and improves error management.
    
    1. Implement `Insert`:
      - Supports inserting elements at any valid index (including appending).
      - Handle automatic capacity growth by appending a zero value.
    
    2. Implement `Range`
      - Provides a traversal mechanism that supports error propagation.
      - Stops iteration immediately if the action function returns an error.
    
    3. Refactor:
      - Introduce sentinel error `ErrIndexOutOfRange`.
      - Replace inline `errors.New("index out of bounds")` with the sentinel error for consistency and testability.

--- list/array_list.go
+++ list/array_list.go
@@ -2,6 +2,8 @@ package list
 
 import "errors"
 
+var ErrIndexOutOfRange = errors.New("ekit: index out of bounds")
+
 type ArrayList[T any] struct {
 	vals []T
 }
@@ -26,7 +28,7 @@ func (a *ArrayList[T]) Append(val T) {
 func (a *ArrayList[T]) Get(index int) (T, error) {
 	if index < 0 || index >= len(a.vals) {
 		var zero T
-		return zero, errors.New("index out of bounds")
+		return zero, ErrIndexOutOfRange
 	}
 	return a.vals[index], nil
 }
@@ -37,7 +39,7 @@ func (a *ArrayList[T]) Delete(index int) (T, error) {
 	length := len(a.vals)
 	if index < 0 || index >= length {
 		var zero T
-		return zero, errors.New("index out of bounds")
+		return zero, ErrIndexOutOfRange
 	}
 	// 2. Capture the value: Save the element to it later.
 	res := a.vals[index]
@@ -61,4 +63,39 @@ func (a *ArrayList[T]) Delete(index int) (T, error) {
 		a.vals = tmp
 	}
 	return res, nil
+}
+
+// Insert inserts a value at the specified index.
+// If the specified index equals length of the list, it appends the value.
+func (a *ArrayList[T]) Insert(index int, val T) error {
+	// 1. Bounds check: Ensure the index is within the valid range.
+	if index < 0 || index > len(a.vals) {
+		return ErrIndexOutOfRange
+	}
+
+	// Grow capacity: Append a zero value to expand the slice by 1.
+	var zero T
+	a.vals = append(a.vals, zero)
+
+	// Shift elements: Move elements from index one step to the right.
+	copy(a.vals[index + 1:], a.vals[index:])
+	// Assign value: Place the new value at the specified index.
+	a.vals[index] = val
+	return nil
+}
+
+
+// Range traverses the list and applies the action function to each element.
+// If the action function return an error, the traverse stops and returns that error.
+func (a *ArrayList[T]) Range(action func(index int, val T) error) error {
+	// Loop through the underlying slice.
+	for index, val := range a.vals {
+		// Invoke the action function.
+		err := action(index, val)
+		// Check error: Return error immediately to stop iteration, if it occurs.
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
